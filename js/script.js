a547c056f71899ca6a46ce0c449f776f3debbeef
MarkusFischerDev <markusfischerdev@gmail.com>
2022-12-03 22:21:19 +0100

    fixed fade-in

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function removeHidden(){
   var element = document.getElementById("hiddenBody");
   var button = document.getElementsByClassName("neon-button")[0]
-  element.removeAttribute("id")
   element.classList.add("fade-in")
+  element.removeAttribute("id")
   button.innerHTML = "INITIALIZING..."
-  button.id = ("hiddenBody")
+  button.classList.add("fade-out")
 }
