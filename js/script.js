05c5748605ffe0bd24e2bc6fb8d72bad7c5a4b2a
MarkusFischerDev <markusfischerdev@gmail.com>
2022-12-03 22:07:34 +0100

    added div1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 function removeHidden(){
   var element = document.getElementById("hiddenBody");
+  var button = document.getElementsByClassName("neon-button")[0]
   element.removeAttribute("id")
-  element.innerHTML = "INITIALIZING..."
-  element.classList.add("hiddenBody")
+  element.classList.add("fade-in")
+  button.innerHTML = "INITIALIZING..."
+  button.id = ("hiddenBody")
 }
