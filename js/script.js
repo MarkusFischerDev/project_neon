1aec7e42f501a282d9d9e7a18efcfd4f79a89a6c
MarkusFischerDev <markusfischerdev@gmail.com>
2022-12-04 14:53:31 +0100

    reworked initialize conten

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 function removeHidden(){
+  var delayInMilliseconds = 2000;
   var element = document.getElementById("hiddenBody");
   var button = document.getElementsByClassName("neon-button")[0]
-  element.classList.add("fade-in")
-  element.removeAttribute("id")
-  button.innerHTML = "INITIALIZING..."
-  button.classList.add("fade-out")
+  button.innerHTML = '<div class="loader">INITIALIZING<p class="loader__dot d-inline">.</p><p class="loader__dot d-inline">.</p><p class="loader__dot d-inline">.</p></div>'
+  setTimeout(function(){
+    button.classList.add("fade-out")
+  }, delayInMilliseconds)
+  setTimeout(function(){
+    element.classList.add("fade-in")
+    element.removeAttribute("id")
+  }, delayInMilliseconds)
 }
