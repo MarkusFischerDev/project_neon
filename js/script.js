c47c3ec8be91e4edf70747a0d24abb75136e14bc
MarkusFischerDev <markusfischerdev@gmail.com>
2022-12-05 11:56:12 +0100

    added fade-in for multiple elements

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 function removeHidden(){
   var delayInMilliseconds = 2000;
   var element = document.getElementById("hiddenBody");
-  var button = document.getElementsByClassName("neon-button")[0]
-  button.innerHTML = '<div class="loader">INITIALIZING<p class="loader__dot d-inline">.</p><p class="loader__dot d-inline">.</p><p class="loader__dot d-inline">.</p></div>'
+  var button = document.getElementsByClassName("neon-button")[0];
+  var fadeInElements = document.querySelectorAll(".revealOnScroll");
+  button.innerHTML = '<div class="loader">INITIALIZING<p class="loader__dot d-inline">.</p><p class="loader__dot d-inline">.</p><p class="loader__dot d-inline">.</p></div>';
   setTimeout(function(){
-    button.classList.add("fade-out")
-  }, delayInMilliseconds)
-  setTimeout(function(){
-    element.classList.add("fade-in-bottom")
-    element.removeAttribute("id")
-  }, delayInMilliseconds)
+    button.classList.add("fade-out");
+    element.classList.add("fade-in-bottom");
+    element.removeAttribute("id");
+  }, delayInMilliseconds);
+
+  function doSetTimeout(i){
+    setTimeout(function(){
+      console.log(fadeInElements);
+      console.log(i)
+      fadeInElements[i].classList.add("fade-in-bottom");
+      fadeInElements[i].classList.remove("revealOnScroll");
+    }, delayInMilliseconds * (i + 2));
+  }
+
+  for(var i = 0; i < fadeInElements.length; i++){
+    doSetTimeout(i)
+  };
 }
